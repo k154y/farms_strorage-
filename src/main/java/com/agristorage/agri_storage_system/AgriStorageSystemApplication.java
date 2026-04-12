df859dbfaba0b86f7ef265cab167c99a8db1378a
k154y <yveskwizera154@gmail.com>
2026-04-12 20:53:05 +0200

    initial commit

---
+++ src/main/java/com/agristorage/agri_storage_system/AgriStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.agristorage.agri_storage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriStorageSystemApplication.class, args);
+	}
+
+}
